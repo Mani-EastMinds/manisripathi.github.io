b1a7ad9e713f51ad9de2c2642606553a5879913e
Mani SRIPATHI <admin@eastminds.com>
2019-02-05 19:18:16 -0600

    Added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SelectComponent } from './components/select/select.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  'path': '', component: SelectComponent, pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
