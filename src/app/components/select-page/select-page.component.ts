fe135f2bc15b1d954ff2ffd36f8d852128e4d503
Mani SRIPATHI <admin@eastminds.com>
2019-02-06 20:27:01 -0600

    Updated components and forms module

--- src/app/components/select-page/select-page.component.ts
+++ src/app/components/select-page/select-page.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./select-page.component.css']
 })
 export class SelectPageComponent implements OnInit {
+  data: { id: number; displayLabel: string; }[];
 
   constructor() { }
 
   ngOnInit() {
+    this.data = [
+      {
+        id: 100001,
+        displayLabel: 'Sports'
+      },
+      {
+        id: 100002,
+        displayLabel: 'Weather'
+      },
+      {
+        id: 100003,
+        displayLabel: 'Local'
+      },
+      {
+        id: 100004,
+        displayLabel: 'International'
+      },
+      {
+        id: 100005,
+        displayLabel: 'Fashion'
+      }
+    ];
   }
 
 }
--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Optional, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
   styleUrls: ['./select.component.css']
+
 })
 export class SelectComponent implements OnInit {
 
-  constructor() { }
+  @Input() id: string;
+  @Input() placeholder: string;
+  @Input() isActive: boolean = false;
+  @Input() isDisabled: boolean = false;
+  @Input() searchable: boolean = false;
+  @Input() isDataProvided: boolean;
+  @Input() data: Array<any> = [];
+  @Input() url: string;
+
+  @Output() onOptionSelect: EventEmitter<any>;
+  optionsData: Array<any> = [];
+  selectedOption: any;
+  toggleDropdown: boolean = false;
+  searchOption: string;
+
+  constructor() {
+    this.onOptionSelect = new EventEmitter<any>();
+  }
 
   ngOnInit() {
+  }
+
+  public toggle(): void {
+    this.toggleDropdown = !this.toggleDropdown;
+  }
+
+  public onSelect(item: any): void {
+    this.selectedOption = item;
+    this.searchOption = item.displayLabel;
+    this.toggle();
+    this.onOptionSelect.emit(item);
   }
 
 }
